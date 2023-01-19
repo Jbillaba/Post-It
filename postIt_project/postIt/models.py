bfd5886cbecf56f7edc2d253ac78c0b36482502c
Jeremy <jeremyvillalva66@gmail.com>
2023-01-19 13:59:19 -0500

    models is added and registered within admin

---
+++ postIt_project/postIt/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    first_name = models.CharField(max_length=30)
+    username = models.CharField(max_length=15, unique=True)
+    email = models.EmailField(max_length=200, unique=True)
+    password = models.CharField(max_length=225)
+
+    def __str__(self):
+        return self.username
+
+class Notes(models.Model):
+    message = models.CharField(max_length=280)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="creator")
+
+    def __str__(self):
+        return self.message
+
+class Board(models.Model):
+    messages = models.ManyToManyField(Notes, related_name="messages")
+
+    def __str__(self) -> str:
+        return self.messages
+
